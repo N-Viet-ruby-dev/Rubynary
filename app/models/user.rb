b43c7dd45af3cbb02a31366a001042717850f246
Duy Nguyen <nguyenngocduy.84@live.com>
2019-07-02 15:53:43 +0700

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   enum role: %i[comtor developer lead_comtor]
 
   validates :name, presence: true, length: { minimum: 5, maximum: 100 }
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Rubynary
     config.load_defaults 5.2
     config.i18n.default_locale = :en
     config.generators.system_tests = nil
+    config.i18n.default_locale = :en
   end
 end
