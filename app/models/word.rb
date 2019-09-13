095608e7b4fd99100b087145b0fa70a614de6610
nguyenvanhung1995 <ghunx1@gmail.com>
2019-09-13 16:23:54 +0700

    Fix import file

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Word < ApplicationRecord
-  validates :ja, presence: true
-  validates :en, presence: true
-  validates :vi, presence: true
+  validates :ja, presence: true, length: { maximum: 255 }
+  validates :en, presence: true, length: { maximum: 255 }
+  validates :vi, presence: true, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 }
+
   has_many :project_words, dependent: :destroy
   has_many :projects, through: :project_words, dependent: :destroy
   has_many :suggested_words, dependent: :destroy
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Word, type: :model do
   it { should validate_presence_of(:en) }
   it { should validate_presence_of(:ja) }
   it { should validate_presence_of(:vi) }
+  it { should validate_length_of(:en).is_at_most(255) }
+  it { should validate_length_of(:ja).is_at_most(255) }
+  it { should validate_length_of(:vi).is_at_most(255) }
+  it { should validate_length_of(:description).is_at_most(255) }
 end
